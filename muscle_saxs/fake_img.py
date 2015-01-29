c3ba6a17021418d45d3696a058ab8f05156af508
Dave Williams <cdave@uw.edu>
2015-01-28 17:23:12 -0800

    Added a symmetric Pearson VII distribution

--- muscle_saxs/fake_img.py
+++ muscle_saxs/fake_img.py
@@ -47,3 +47,24 @@ def background(size, center, a, b, c, d, e):
     back = lambda x,y: a + exp(x,y,b,c) + exp(x,y,d,e)
     img = evaluate_to_img(back, size)
     return img
+
+def pearson(size, center, H, K, M):
+    """Return an image with a symmetrical Pearson VII distribution.
+    This is defined as:
+        pear = H * (1 + K**2 * dist**2 / M)**-M
+    Where the distance is from the passed center.
+    Takes:
+        size - (row, col) size of the image to generate
+        center - the (row, col) center of the background image
+        H - the height of the distribution (should be >0)
+        K - controls the spread (should be >0)
+        M - controls the rate of decay of the tails (should be >0)
+    Returns:
+        img - the generated distribution image
+    """
+    dist = lambda x,y: np.hypot((center[0]-x), (center[1]-y))
+    sq = lambda x: np.power(x,2)
+    pear = lambda x,y: H * np.power(1 + (sq(K) * sq(dist(x,y))) / M, -M)
+    img = evaluate_to_img(pear, size)
+    return img
+
