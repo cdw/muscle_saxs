ecbc1d4c7e20840eb21d0795575d46bbac0f1122
Dave Williams <cdave@uw.edu>
2015-06-25 16:47:57 -0700

    Handle case where no peaks are found

--- muscle_saxs/peak_finder.py
+++ muscle_saxs/peak_finder.py
@@ -227,6 +227,9 @@ def extract_d10(pairs, horizontal = True, plot = False, pimg = None):
     # Find distances, choose smallest
     d_f = lambda p: np.hypot(p[0][0]-p[0][1], p[1][0]-p[1][1]) # dist func
     dists = np.array(map(d_f, pairs))
+    if len(dists) == 0:
+        # no d10 found
+        return np.NaN
     d10 = pairs[np.argmin(dists)]
     # Plot if option to plot passed
     if plot is not False:
